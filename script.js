91709cdf4d2c1ec20898383f5ebba53404bb1b47
Bel Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-07-21 20:55:51 -0300

    requisito 21

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ buttonLogin.addEventListener('click', confirmaLogin);
 const buttonForm = document.querySelector('#submit-btn');
 const checkagreement = document.querySelector('#agreement');
 
+buttonForm.addEventListener('click', preventDef);
+
 function ativarButton() {
   if (checkagreement.checked === true) {
     buttonForm.disabled = false;
@@ -45,3 +47,43 @@ function contador() {
 }
 
 textarea.addEventListener('input', contador);
+
+const subjectArray = [];
+
+function subjectChecked() {
+  const subject = document.querySelectorAll('input[name="subject"]');
+  for (let index of subject) {
+    if (index.checked === true) {
+      subjectArray.push(index.value);
+    }
+  }
+  return subjectArray.join(', ');
+}
+
+function divInputValue(valorInput) {
+  const novaDiv = document.createElement('div');
+  novaDiv.innerHTML = valorInput;
+  return novaDiv;
+}
+
+function funçaoFinal() {
+  const nome = document.querySelector('#input-name').value;
+  const sobrenome = document.querySelector('#input-lastname').value;
+  const email = document.querySelector("#input-email").value;
+  const casa = document.querySelector('#house').value;
+  const familia = document.querySelector('input[name="family"]:checked').value;
+  const materias = subjectChecked();
+  const avaliacao = document.querySelector('input[name="rate"]:checked').value;
+  const observaçao = textarea.value;
+  const form = document.querySelector('#evaluation-form');
+  form.innerHTML = '';
+  form.appendChild(divInputValue('Nome: ' + nome + ' ' + sobrenome));
+  form.appendChild(divInputValue('Email: ' + email));
+  form.appendChild(divInputValue('Casa: ' + casa));
+  form.appendChild(divInputValue('Família: ' + familia));
+  form.appendChild(divInputValue('Matérias: ' + materias));
+  form.appendChild(divInputValue('Avaliação: ' + avaliacao));
+  form.appendChild(divInputValue('Observações: ' + observaçao));
+}
+
+buttonForm.addEventListener('click', funçaoFinal)
